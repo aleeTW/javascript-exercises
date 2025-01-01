bb651d6a0ac76e247092bbd4f73f368fe322c89e
threenineseventwo <alvinlee020706@gmail.com>
2025-01-01 15:51:09 -0500

    Problems 5-7

--- 05_sumAll/README.md
+++ 05_sumAll/README.md
@@ -1,5 +1,6 @@
 # Exercise 05 - sumAll
 
+
 Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 
 ```javascript
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (valueOne, valueTwo) {
+  let sum = 0;
+  if (Number.isInteger(valueOne) && Number.isInteger(valueTwo)) {
+    if (valueOne > 0 && valueTwo > 0) {
+      if (valueOne < valueTwo) {
+        for (valueOne; valueOne <= valueTwo; valueOne++) {
+          sum += valueOne;
+        }
+      } else {
+        for (valueTwo; valueTwo <= valueOne; valueTwo++) {
+          sum += valueTwo;
+        }
+      }
+    } else {
+      return "ERROR";
+    }
+  } else {
+    return "ERROR";
+  }
+  return sum;
 };
 
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
