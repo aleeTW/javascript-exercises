aa9d6b6f9f9e0efd3518d2f4adbff1f3318e3884
threenineseventwo <alvinlee020706@gmail.com>
2025-01-01 15:22:39 -0500

    Problems 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, repeat) {
+  let total = "";
+  if (repeat < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < repeat; i++) {
+      total += text;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...value) {
+  const newArray = [];
+
+  array.forEach((element) => {
+    if (!value.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 2, 7));
 // Do not edit below this line
 module.exports = removeFromArray;
